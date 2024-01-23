8a523e8de159170f6334129c7e0cf5db7152f2cf
BorsosIstvan <bistvanus@gmail.com>
2024-01-23 13:34:56 +0100

    Hoi

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request, redirect, url_for
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    username = request.form['username']
+    password = request.form['password']
+
+    # Load existing user data from JSON file
+    try:
+        with open('users.json', 'r') as file:
+            users = json.load(file)
+    except FileNotFoundError:
+        users = []
+
+    # Check if the username is already taken
+    for user in users:
+        if user['username'] == username:
+            return 'Username already taken!'
+
+    # Add the new user to the list
+    new_user = {'username': username, 'password': password}
+    users.append(new_user)
+
+    # Save the updated user data to the JSON file
+    with open('users.json', 'w') as file:
+        json.dump(users, file, indent=2)
+
+    return redirect(url_for('home'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
